47e4f3efdbef0cdaff3bd23e52661bca46ee951c
bagus2x <dutapanggulu@gmail.com>
2021-01-07 18:27:05 +0700

    add options method & fix typo

--- sumux.go
+++ sumux.go
@@ -38,8 +38,8 @@ func NewServeSumux() *ServeSumux {
 	return &ServeSumux{pcm: pcm}
 }
 
-func (sg ServeSumux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	for _, pc := range sg.pcm[r.Method] {
+func (s ServeSumux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, pc := range s.pcm[r.Method] {
 		if equalPath(r.URL.Path, pc.pathXp) {
 			ctx := context.WithValue(r.Context(), pathWithKey, pc.path)
 			pc.f(w, r.WithContext(ctx))
@@ -51,40 +51,46 @@ func (sg ServeSumux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 // Get -
-func (sg *ServeSumux) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	path = concatPath(sg.groupPath, path)
-	sg.pcm["GET"] = append(sg.pcm["GET"], PathCallback{path, compiledPath(path), f})
+func (s *ServeSumux) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	path = concatPath(s.groupPath, path)
+	s.pcm["GET"] = append(s.pcm["GET"], PathCallback{path, compiledPath(path), f})
 }
 
 // Post -
-func (sg *ServeSumux) Post(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	path = concatPath(sg.groupPath, path)
-	sg.pcm["POST"] = append(sg.pcm["POST"], PathCallback{path, compiledPath(path), f})
+func (s *ServeSumux) Post(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	path = concatPath(s.groupPath, path)
+	s.pcm["POST"] = append(s.pcm["POST"], PathCallback{path, compiledPath(path), f})
 }
 
 // Put -
-func (sg *ServeSumux) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	path = concatPath(sg.groupPath, path)
-	sg.pcm["PUT"] = append(sg.pcm["PUT"], PathCallback{path, compiledPath(path), f})
+func (s *ServeSumux) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	path = concatPath(s.groupPath, path)
+	s.pcm["PUT"] = append(s.pcm["PUT"], PathCallback{path, compiledPath(path), f})
 }
 
 // Delete -
-func (sg *ServeSumux) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	path = concatPath(sg.groupPath, path)
-	sg.pcm["DELETE"] = append(sg.pcm["DELETE"], PathCallback{path, compiledPath(path), f})
+func (s *ServeSumux) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	path = concatPath(s.groupPath, path)
+	s.pcm["DELETE"] = append(s.pcm["DELETE"], PathCallback{path, compiledPath(path), f})
 }
 
 // Patch -
-func (sg *ServeSumux) Patch(path string, f func(w http.ResponseWriter, r *http.Request)) {
-	path = concatPath(sg.groupPath, path)
-	sg.pcm["PATCH"] = append(sg.pcm["PATCH"], PathCallback{path, compiledPath(path), f})
+func (s *ServeSumux) Patch(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	path = concatPath(s.groupPath, path)
+	s.pcm["PATCH"] = append(s.pcm["PATCH"], PathCallback{path, compiledPath(path), f})
+}
+
+// Options -
+func (s *ServeSumux) Options(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	path = concatPath(s.groupPath, path)
+	s.pcm["OPTIONS"] = append(s.pcm["OPTIONS"], PathCallback{path, compiledPath(path), f})
 }
 
 // Group -
-func (sg *ServeSumux) Group(path string, f func(r Router)) {
-	sg.groupPath += path
-	f(sg)
-	sg.groupPath = ""
+func (s *ServeSumux) Group(path string, f func(r Router)) {
+	s.groupPath += path
+	f(s)
+	s.groupPath = ""
 }
 
 func compiledPath(path string) string {
